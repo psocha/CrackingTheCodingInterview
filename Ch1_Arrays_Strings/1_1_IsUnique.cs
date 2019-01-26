d36d1c11bde5d2406c3b08937071369b5cf418e9
Peter Socha <psocha@uwaterloo.ca>
2019-01-25 21:37:30 -0800

    Problem 1.1 with project setup

---
+++ Ch1_Arrays_Strings/1_1_IsUnique.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI
+{
+    public class StringUniquenessChecker
+    {
+        /**
+         * Is Unique: Implement an algorithm to determine if a string has all unique characters.
+         */
+        public static bool IsStringAllUniqueChars(string str)
+        {
+            var characterSet = new HashSet<char>();
+
+            foreach (char c in str)
+            {
+                if (characterSet.Contains(c))
+                {
+                    return false;
+                }
+                characterSet.Add(c);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Ch1_Tests/1_1_IsUnique_Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CTCI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ch1_Tests
+{
+    [TestClass]
+    public class StringUniquenessCheckerTest
+    {
+        [TestMethod]
+        public void IsUniqueTest()
+        {
+            Assert.IsTrue(StringUniquenessChecker.IsStringAllUniqueChars("qwertyuiopasdfghjklzxcvbnm"));
+            Assert.IsFalse(StringUniquenessChecker.IsStringAllUniqueChars("asdfa"));
+            Assert.IsTrue(StringUniquenessChecker.IsStringAllUniqueChars(""));
+            Assert.IsFalse(StringUniquenessChecker.IsStringAllUniqueChars("ZZZZZZZZZZZZZZZZZZZZZ"));
+        }
+    }
+}
